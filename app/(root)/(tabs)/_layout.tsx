ac05709f55b5d94ff49cdfee0d8feeb8e89ba0bf
Vamshi Adimalla <vamshiadimalla@Vamshis-MacBook-Air.local>
2025-03-04 21:40:47 +0530

    Most frontend completed

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,65 +1,39 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Tabs } from 'expo-router'
+import { Stack } from 'expo-router'
+import Drawer from 'expo-router/drawer'
+import { useUser } from '@clerk/clerk-expo'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons'
 
-const TabIcon = ({ name, focused } : { name: any, focused: boolean}) => {
-     return (
-          <View className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-outline" : ""}`}>
-               <View className={`rounded-full w-14 h-14 justify-center items-center ${focused ? "bg-outline" : ""}`}>
-                    <Ionicons name={name} size={focused ? 30 : 20} color={focused ? "white" : "white"} />
-               </View>
-          </View>
-     )
+const CustomDrawer = () => {
+  const { user } = useUser();
+  const { bottom, top } = useSafeAreaInsets();
+  return (
+    <View style={{flex: 1, marginTop: top}} className='bg-black h-full w-full'>
+      <View className='pb-4'>
+        <Text className='text-lg text-white'>{user?.emailAddresses[0].emailAddress}</Text>
+      </View>
+    </View>
+  )
 }
+
 const _layout = () => {
   return (
-    <Tabs initialRouteName='index' screenOptions={{
-     tabBarActiveTintColor: "white",
-     tabBarInactiveTintColor: "white",
-     tabBarShowLabel: false,
-     tabBarStyle: {
-          backgroundColor: "#333333",
-          borderRadius: 50,
-          paddingBottom: 0,
-          marginHorizontal: 20,
-          marginBottom: 20,
-     }
-     
-    }}>
-     <Tabs.Screen 
-          name="home" 
-          options={{
-               title: "Home",
-               headerShown: false,
-               tabBarIcon: ({focused}) => <TabIcon name="home-outline" focused={focused} />
-          }}
-     />
-     <Tabs.Screen 
-          name="orders" 
-          options={{
-               title: "Orders",
-               headerShown: false,
-               tabBarIcon: ({focused}) => <TabIcon name="cube-outline" focused={focused} />
-          }}
-     />
-     <Tabs.Screen 
-          name="chat" 
-          options={{
-               title: "Chat",
-               headerShown: false,
-               tabBarIcon: ({focused}) => <TabIcon name="chatbox-outline" focused={focused} />
-          }}
-     />
-     <Tabs.Screen 
-          name="profile" 
-          options={{
-               title: "Profile",
-               headerShown: false,
-               tabBarIcon: ({focused}) => <TabIcon name="person-outline" focused={focused} />
-          }}
-     />
-    </Tabs> 
+    <Stack>
+      <Stack.Screen name="home" options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="petrol" options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="diesel" options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="orders" options={{
+        headerShown: false
+      }} />
+    </Stack>
   )
 }
 
--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,17 +1,67 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { SafeAreaView, Text, View } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import FuelCard from '@/components/FuelCard'
+import CustomButton from '@/components/CustomButton'
+import { Ionicons } from '@expo/vector-icons'
+import ReactNativeModal from 'react-native-modal'
+import { Redirect, router } from 'expo-router'
 
-
-export default function Page() {
-  const { user } = useUser()
-
+const home = () => {
+  const [show, setShow] = useState(false);
   return (
-    <SafeAreaView>
-      <SignedIn>
-        <View className='justify-center items-center w-full h-full'>
-          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+    <View className='h-full w-full bg-slate-300'>
+      <View className='justify-center items-center mt-20'>
+        <Text className='text-4xl font-bold'>FuelFlex</Text>
+      </View>
+      <View className='flex items-center justify-center gap-y-4 mt-10'>
+        <FuelCard
+          fuel="Diesel"
+          price={109}
+        />
+        <FuelCard
+          fuel="Petrol"
+          price={129}
+        />
+      </View>
+      <View className='absolute self-center bottom-0 w-80'>
+        <View className='flex-row justify-center items-center mb-2'>
+          <TouchableOpacity 
+            className='bg-outline rounded-full w-full p-3 items-center justify-center'
+            onPress={() => setShow(true)}
+          >
+            <Text className='text-white font-bold'>Order</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="cube-outline" size={35} />
+          </TouchableOpacity>
         </View>
-      </SignedIn>
-    </SafeAreaView>
+        <ReactNativeModal isVisible={show}>
+          <View className='bg-white px-7 py-9 rounded-2xl min-h-[300px]'>
+            <TouchableOpacity className='self-end' onPress={() => setShow(false)}>
+              <Ionicons name="close-outline" size={30} />
+            </TouchableOpacity>
+            <Text className='text-center font-bold text-xl'>Order</Text>
+            <CustomButton
+                title="Petrol"
+                onPress={() => {
+                  router.push("/(root)/(tabs)/petrol")
+                }}
+                className='mt-5'
+              />
+            <CustomButton
+              title="Diesel"
+              onPress={() => {
+                router.push("/(root)/(tabs)/diesel")
+              }}
+              className='mt-5'
+            />
+          </View>
+        </ReactNativeModal>
+        <View>
+        </View>
+      </View>
+    </View>
   )
 }
+
+export default home
